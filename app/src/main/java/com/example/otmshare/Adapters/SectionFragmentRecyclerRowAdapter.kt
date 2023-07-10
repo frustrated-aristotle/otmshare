585b9410f7b169521bff32f68c0d1e23d70aaceb
Abdullah Seçgeler <a.secgeler.imp@gmail.com>
2023-07-10 17:38:02 +0300

    Save button is fully functional. Commit before making private fun likeImage more generic and reading the user's saved and liked.

--- app/src/main/java/com/example/otmshare/Adapters/SectionFragmentRecyclerRowAdapter.kt
+++ app/src/main/java/com/example/otmshare/Adapters/SectionFragmentRecyclerRowAdapter.kt
@@ -2,16 +2,15 @@ package com.example.otmshare.Adapters
 
 import android.animation.ObjectAnimator
 import android.animation.PropertyValuesHolder
+import android.annotation.SuppressLint
 import android.content.Intent
-import android.content.res.ColorStateList
 import android.net.Uri
-import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.ui.graphics.Color
-import androidx.core.content.ContentProviderCompat.requireContext
+import android.widget.ImageView
+import androidx.cardview.widget.CardView
 import androidx.core.content.ContextCompat.startActivity
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
@@ -21,8 +20,6 @@ import com.example.otmshare.Sections.Section
 import com.example.otmshare.databinding.SectionFragmentRecyclerRowBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.SetOptions
-import kotlin.math.exp
 
 class SectionFragmentRecyclerRowAdapter(var sectionList : MutableList<Section>) : RecyclerView.Adapter<SectionFragmentRecyclerRowAdapter.SectionViewHolder>(){
 
@@ -42,7 +39,10 @@ class SectionFragmentRecyclerRowAdapter(var sectionList : MutableList<Section>) 
         return sectionList.size
     }
 
+    @SuppressLint("ClickableViewAccessibility")
     override fun onBindViewHolder(holder: SectionViewHolder, position: Int) {
+        //TODO:
+
         holder.view.titleText.text = makeItATitle(sectionList[position].seasonAndEpisode)
         holder.view.contentText.text = sectionList[position].content
         holder.view.urlText.text = sectionList[position].url
@@ -51,69 +51,6 @@ class SectionFragmentRecyclerRowAdapter(var sectionList : MutableList<Section>) 
             val podcastUrl = sectionList[position].url
             openSpotifyPodcast(podcastUrl, it)
         }
-        val scaleDown = ObjectAnimator.ofPropertyValuesHolder(
-            holder.view.cardView2,
-            PropertyValuesHolder.ofFloat(View.SCALE_X, 0.9f),
-            PropertyValuesHolder.ofFloat(View.SCALE_Y, 0.9f)
-        ).apply {
-            duration = 100
-            repeatCount = 0
-            repeatMode = ObjectAnimator.REVERSE
-        }
-        val scaleDownLike = ObjectAnimator.ofPropertyValuesHolder(
-            holder.view.likeImage,
-            PropertyValuesHolder.ofFloat(View.SCALE_X, 0.9f),
-            PropertyValuesHolder.ofFloat(View.SCALE_Y, 0.9f)
-        ).apply {
-            duration = 100
-            repeatCount = 0
-            repeatMode = ObjectAnimator.REVERSE
-        }
-        val c = android.graphics.Color.parseColor("#D3D2D2")
-        holder.view.cardView2.setOnTouchListener { view, motionEvent ->
-            when (motionEvent.action) {
-                MotionEvent.ACTION_DOWN -> {
-                    // CardView dokunulduğunda animasyonu başlat
-                    scaleDown.start()
-                    holder.view.cardView2.setCardBackgroundColor(c)
-                }
-                MotionEvent.ACTION_CANCEL -> {
-                    // CardView dokunuşu serbest bırakıldığında veya iptal edildiğinde animasyonu durdur
-                    scaleDown.cancel()
-                    holder.view.cardView2.scaleX = 1.0f
-                    holder.view.cardView2.scaleY = 1.0f
-                    holder.view.cardView2.setCardBackgroundColor(android.graphics.Color.WHITE)
-                }
-                MotionEvent.ACTION_UP ->{
-                    scaleDown.cancel()
-                    holder.view.cardView2.scaleX = 1.0f
-                    holder.view.cardView2.scaleY = 1.0f
-                    val podcastUrl = sectionList[position].url
-                    openSpotifyPodcast(podcastUrl, holder.view.cardView2)
-                    holder.view.cardView2.setCardBackgroundColor(android.graphics.Color.WHITE)
-                }
-            }
-            true
-        }
-        holder.view.likeImage.setOnTouchListener { view, motionEvent -> when (motionEvent.action) {
-            MotionEvent.ACTION_DOWN -> {
-                // CardView dokunulduğunda animasyonu başlat
-                scaleDownLike.start()
-            }
-            MotionEvent.ACTION_CANCEL -> {
-                // CardView dokunuşu serbest bırakıldığında veya iptal edildiğinde animasyonu durdur
-                scaleDownLike.cancel()
-                holder.view.likeImage.scaleX = 1.0f
-                holder.view.likeImage.scaleY = 1.0f
-              //  holder.view.cardView2.setCardBackgroundColor(android.graphics.Color.WHITE)
-            }
-            MotionEvent.ACTION_UP ->{
-                scaleDownLike.cancel()
-                holder.view.likeImage.scaleX = 1.0f
-                holder.view.likeImage.scaleY = 1.0f
-            }
-        }
-            true }
         holder.view.likeImage.setOnClickListener{
             likeImage(sectionList[position].id)
             println("Click on like")
@@ -121,8 +58,97 @@ class SectionFragmentRecyclerRowAdapter(var sectionList : MutableList<Section>) 
         holder.view.saveImage.setOnClickListener{
             println("Click on save")
         }
+        animateImages(listOf(holder.view.likeImage, holder.view.saveImage),sectionList[position].id)
+
+        animateCardView(holder.view.cardView2, position)
+
+
         //holder.view.listener = this!!
     }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private fun animateCardView(cardView : CardView, position: Int) {
+        val c = android.graphics.Color.parseColor("#D3D2D2")
+        val scaleDown = ObjectAnimator.ofPropertyValuesHolder(
+            cardView,
+            PropertyValuesHolder.ofFloat(View.SCALE_X, 0.9f),
+            PropertyValuesHolder.ofFloat(View.SCALE_Y, 0.9f)
+        ).apply {
+            duration = 100
+            repeatCount = 0
+            repeatMode = ObjectAnimator.REVERSE
+        }
+        cardView.setOnTouchListener { view, motionEvent ->
+            when (motionEvent.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    // CardView dokunulduğunda animasyonu başlat
+                    scaleDown.start()
+                    cardView.setCardBackgroundColor(c)
+                }
+                MotionEvent.ACTION_CANCEL -> {
+                    // CardView dokunuşu serbest bırakıldığında veya iptal edildiğinde animasyonu durdur
+                    scaleDown.cancel()
+                    cardView.scaleX = 1.0f
+                    cardView.scaleY = 1.0f
+                    cardView.setCardBackgroundColor(android.graphics.Color.WHITE)
+                }
+                MotionEvent.ACTION_UP ->{
+                    scaleDown.cancel()
+                    cardView.scaleX = 1.0f
+                    cardView.scaleY = 1.0f
+                    val podcastUrl = sectionList[position].url
+                    openSpotifyPodcast(podcastUrl, cardView)
+                    cardView.setCardBackgroundColor(android.graphics.Color.WHITE)
+                }
+            }
+            true
+        }
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private fun animateImages(imgViewList: List<ImageView>, id : Long)
+    {
+        var imgView : ImageView
+        for (i in 0..1)
+        {
+            imgView = imgViewList[i]
+            //Scale Down:
+            val scaleDown = ObjectAnimator.ofPropertyValuesHolder(
+                imgView,
+                PropertyValuesHolder.ofFloat(View.SCALE_X, 0.9f),
+                PropertyValuesHolder.ofFloat(View.SCALE_Y, 0.9f)
+            ).apply {
+                duration = 100
+                repeatCount = 0
+                repeatMode = ObjectAnimator.REVERSE
+            }
+            //onTouchListener
+            imgView.setOnTouchListener { view, motionEvent ->
+                when (motionEvent.action) {
+                    MotionEvent.ACTION_DOWN -> {
+                        scaleDown.start()
+                    }
+                    MotionEvent.ACTION_CANCEL -> {
+                        scaleDown.cancel()
+                        imgView.scaleX = 1.0f
+                        imgView.scaleY = 1.0f
+                    }
+                    MotionEvent.ACTION_UP ->{
+                        scaleDown.cancel()
+                        imgView.scaleX = 1.0f
+                        imgView.scaleY = 1.0f
+                        if (i == 0) //like Img
+                            {view.background = view.context.getDrawable(R.drawable.baseline_thumb_up_alt_24)
+                            likeImage(id)}
+                        else
+                            view.background = view.context.getDrawable(R.drawable.baseline_turned_in_24)
+                    }
+                }
+                true
+            }
+        }
+    }
+
 
     private fun likeImage(id: Long) {
         val currentUser = auth.currentUser
@@ -131,6 +157,7 @@ class SectionFragmentRecyclerRowAdapter(var sectionList : MutableList<Section>) 
         collectionRef.get()
             .addOnSuccessListener { documents ->
                 for (document in documents) {
+
                     val documentId = document.id
                     println("Doc ID : " + documentId +  " " +id)
                     val favSections = document.get("favouriteSections") as String
